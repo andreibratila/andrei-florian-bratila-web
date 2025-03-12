c55b2e46dda6619747fdd717fd215cadd686d595
Andrei Florian Bratila <andreiflorianbratila@gmail.com>
2025-03-12 02:16:25 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,40 @@
 import createMiddleware from 'next-intl/middleware';
-import { NextRequest, NextResponse } from 'next/server';
 
-import { LocaleI } from './i18n';
-// Necesario para las redirecciones
+// import { NextRequest, NextResponse } from 'next/server';
+
 import { routing } from './i18n/routing';
 
-const supportedLocales = routing.locales; // Idiomas válidos
-const defaultLocale = routing.defaultLocale; // Idioma por defecto
+export default createMiddleware(routing);
 
-export default function customMiddleware(req: NextRequest) {
-  const url = req.nextUrl; // Obtenemos la URL de la solicitud
-  const pathname = url.pathname; // Ruta solicitada
+// import { LocaleI } from './i18n';
+// // Necesario para las redirecciones
 
-  // Obtener el idioma del primer segmento de la URL
-  const locale = pathname.split('/')[1];
+// const supportedLocales = routing.locales; // Idiomas válidos
+// const defaultLocale = routing.defaultLocale; // Idioma por defecto
 
-  // Si el idioma no está en los idiomas soportados
-  if (locale && !supportedLocales.includes(locale as LocaleI)) {
-    // Redirigir a la página principal (/) o al idioma predeterminado (/es)
-    return NextResponse.redirect(new URL(`/${defaultLocale}`, req.url));
-  }
+// export default function customMiddleware(req: NextRequest) {
+//   const url = req.nextUrl; // Obtenemos la URL de la solicitud
+//   const pathname = url.pathname; // Ruta solicitada
 
-  // Si el idioma es válido o no hay un idioma definido, continuar con el middleware normal
-  return createMiddleware(routing)(req);
-}
+//   // Obtener el idioma del primer segmento de la URL
+//   const locale = pathname.split('/')[1];
+
+//   // Si el idioma no está en los idiomas soportados
+//   if (locale && !supportedLocales.includes(locale as LocaleI)) {
+//     // Redirigir a la página principal (/) o al idioma predeterminado (/es)
+//     return NextResponse.redirect(new URL(`/${defaultLocale}`, req.url));
+//   }
+
+//   // Si el idioma es válido o no hay un idioma definido, continuar con el middleware normal
+//   return createMiddleware(routing)(req);
+// }
 
 export const config = {
-  // Match all paths except aquellos que no deberían ser internacionalizados
-  matcher: ['/((?!api|_next|_vercel|.*/opengraph-image|.*\\..*).*)'],
+  // Match only internationalized pathnames
+  matcher: ['/', '/(es|en|ro|ca)/:path*'],
 };
+
+// export const config = {
+//   // Match all paths except aquellos que no deberían ser internacionalizados
+//   matcher: ['/((?!api|_next|_vercel|.*/opengraph-image|.*\\..*).*)'],
+// };
